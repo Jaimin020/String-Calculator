83253587aff070db2e43a93910d0cca70c381546
Jaimin020 <48099245+Jaimin020@users.noreply.github.com>
2021-03-05 21:38:26 +0530

    Update in Add method
    Add() method is updated to handle addtion of more than 2 digits.

--- Add_Numbers/src/String_Calculator/StringCalculator.java
+++ Add_Numbers/src/String_Calculator/StringCalculator.java
@@ -11,13 +11,11 @@ public class StringCalculator {
 		else
 		{
 			StringTokenizer st = new StringTokenizer(numbers,",");
-			int sum,n1=0,n2=0;
-			n1 = Integer.parseInt(st.nextToken());
-			if(st.hasMoreTokens())
+			int sum=0;
+			while(st.hasMoreTokens())
 			{
-				n2 = Integer.parseInt(st.nextToken());
+				sum+=Integer.parseInt(st.nextToken());
 			}
-			sum=n1+n2;
 			return(sum);
 		}
 	}
